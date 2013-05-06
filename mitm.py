ebbd3b1161614ce2f7a13374b3a71f8b1e2ee6a7
Joe Boyle <git@sublimation.org>
2013-05-06 16:09:08 -0700

    still more simplifications

--- mitm.py
+++ mitm.py
@@ -58,43 +58,6 @@ class NetfilterQueueWrapper(NetfilterQueue):
         """This destructor just calls the named cleanup function"""
         self.cleanup()
 
-class ConnectionManager():
-    """Object to select an active socket connection and divert it to the netfilter queue"""
-    def __init__(self):
-        self.refresh()
-
-    def refresh(self):
-        """this function will freshen the connect listing."""
-        p = subprocess.Popen('netstat -W -n -a -A inet | grep ESTABLISHED', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        retval=p.wait()
-        self.connections=map(lambda line: line.rstrip(), p.stdout.readlines())
-        
-    def netstatSelect(self):
-        """This function will prompt the user for a locally terminated connection using netstat."""
-        if len(self.connections) == 0:
-            print 'No active connections detected.'
-            return
-
-        # show the netstat output as numbered lines
-        lineNumber=1
-        for line in self.connections:
-            print str(lineNumber) + ': ' + line
-            lineNumber+=1
-
-        print "Select line number: "
-        selectedLine=int(raw_input()) - 1
-
-        #tcp        0      0 172.16.65.148:ssh       172.16.65.1:53698       ESTABLISHED
-        proto, ignore1, ignore2, dst, src, ignore3 = self.connections[selectedLine].split()
-        rip,rport = src.split(':')
-        lip,lport = dst.split(':')
-
-        # create two dictionaries and return them
-        remote={ 'table':'OUTPUT', 'ip':rip, 'proto':proto, 'port':rport }
-        local= { 'table':'INPUT',  'ip':lip, 'proto':proto, 'port':lport }
-        return local, remote
-
-
 class analyzer():
     def __init__(self):
         self.last=None
@@ -114,15 +77,41 @@ class analyzer():
     def induce_jitter(self):
         pass
 
-try:
-    # create a connection manager 
-    CM=ConnectionManager()
+def netstatSelect():
+    """This function will prompt the user for a locally terminated connection using netstat."""
+    p = subprocess.Popen('netstat -W -n -a -A inet | grep ESTABLISHED', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    retval=p.wait()
+    connections=map(lambda line: line.rstrip(), p.stdout.readlines())
 
+    if len(connections) == 0:
+        print 'No active connections detected.'
+        return
+
+    # show the netstat output as numbered lines
+    lineNumber=1
+    for line in connections:
+        print str(lineNumber) + ': ' + line
+        lineNumber+=1
+
+    print "Select line number: "
+    selectedLine=int(raw_input()) - 1
+
+    #tcp        0      0 172.16.65.148:ssh       172.16.65.1:53698       ESTABLISHED
+    proto, ignore1, ignore2, dst, src, ignore3 = connections[selectedLine].split()
+    rip,rport = src.split(':')
+    lip,lport = dst.split(':')
+
+    # create two dictionaries and return them
+    remote={ 'table':'OUTPUT', 'ip':rip, 'proto':proto, 'port':rport }
+    local= { 'table':'INPUT',  'ip':lip, 'proto':proto, 'port':lport }
+    return local, remote
+
+try:
     # create an analyzer object 
     a=analyzer()
 
     # use the netstatSelect method to select a locally terminated connection
-    local, remote = CM.netstatSelect()
+    local, remote = netstatSelect()
 
     # assign the callback Functions
     local['callback'] = a.measure_jitter
